580ad19b7cdb9b1916a797b2a54426f376cb2930
Azrubael <i0638464000@gmail.com>
2022-11-17 11:56:37 +0200

    Added styles .scss minimizator with dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // first import ...
+// first import
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// styles minimizator
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+module.exports = (env={}, argv={}) => ({
   module: {
     rules: [
 	{
@@ -25,12 +29,42 @@ module.exports = {
 	},
 	{
 		test: /\.scss$/,
-		use: ['style-loader', 'css-loader', 'sass-loader']
+		// use: ['style-loader', 'css-loader', 'sass-loader']
+		use: [
+			argv.mode === "production"
+			? MiniCssExtractPlugin.loader
+			: "style-loader",
+			{
+				loader: "css-loader", options: {
+					sourceMap: true
+				}
+			},
+			{
+				loader: "sass-loader", options: {
+					sourceMap: true
+				}
+			}
+		]
 	}]
   },
   devtool: "source-map",
   // target: 'web', 			//added from stackOverflow
   plugins: [
-    new HtmlWebpackPlugin() // ... then register it
-  ]
-};
+	// Any option given to Webpack client can be captured on the "argv"
+    /* А эта пропись нужна для того, чтобы выключить генерацию index.html,
+	    если это уже не нужно для режима "production"
+		argv.mode === "development" ? new HtmlWebpackPlugin() : null,
+		*/
+	new HtmlWebpackPlugin(),
+	argv.mode === "production"
+		? new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+			})
+		: null
+  ].filter(
+	// To remove any possibility of "null" values
+	// inside the plugins array, we filter it
+	plugin => !!plugin
+  )
+});
