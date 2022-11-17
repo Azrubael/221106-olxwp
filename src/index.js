fcf9d226fa54acb91d423adbe79f11291cdcae4a
Azrubael <i0638464000@gmail.com>
2022-11-17 10:18:23 +0200

    Added styles with .scss and a few of dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import hamster from "../assets/0fb35409-1df9-4d1b-ac84-42b23f7ff363.jpg"
 import Sail from "../assets/sailingTheme.mp3"
+import "./style.scss"
 import { hello } from "./hello"
 hello("OLX Dev!")
 
@@ -8,10 +9,9 @@ const audio = new Audio(Sail)
 const pic = document.querySelector("body")
 
 document.body.innerHTML = '<div id="myMemes"></div>'
-document.getElementById("myMemes").innerHTML = `<center>
-	<h1>And his name is...</h1>
-	<img id="pic" src="${hamster}" height="250px" /><br />
-	<h2><i>HAMSTER</i></h2></center>`
+document.getElementById("myMemes").innerHTML = `<h1>And his name is...</h1>
+	<img id="pic" src="${hamster}" /><br />
+	<h2><i>HAMSTER</i></h2>`
 	
 pic.addEventListener('click', () => {
 	console.log('Клик!')
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
 	{
 		test: /\.(ogg|mp3|wav|mpe?g)$/i,
 		use: 'file-loader'
+	},
+	{
+		test: /\.scss$/,
+		use: ['style-loader', 'css-loader', 'sass-loader']
 	}]
   },
   devtool: "source-map",
